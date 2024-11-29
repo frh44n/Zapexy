091a3efcb70398578d2dc1a6855ec9474eb7b35c
frh44n <107492548+frh44n@users.noreply.github.com>
2024-11-30 05:04:45 +0530

    Update start.py

--- start.py
+++ start.py
@@ -1,7 +1,7 @@
 import logging
 from telegram import InlineKeyboardButton, InlineKeyboardMarkup, Update
 from telegram.ext import Updater, CommandHandler, MessageHandler, CallbackQueryHandler, CallbackContext
-from telegram.ext.filters import Filters
+from telegram.ext import filters
 from flask import Flask, request
 import os
 
